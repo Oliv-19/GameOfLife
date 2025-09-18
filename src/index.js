ec8068a384e275ffb67f11ca704886677d9f4e6b
Oliv-19 <betza.bruguera@gmail.com>
2025-09-18 09:35:37 -0400

    Add restart button and animation

--- src/index.js
+++ src/index.js
@@ -3,26 +3,41 @@ import "./style.css";
 (function init(){
     const startBtn = document.querySelector('.start')
     const stopBtn = document.querySelector('.stop')
+    const restartBtn = document.querySelector('.restart')
     const grid = document.querySelector('.grid')
     const boardInstance = new Board()
     let board= boardInstance.randomState(Math.floor(grid.clientWidth*0.08), Math.floor(grid.clientHeight*0.07))
     let stop=false
-    stopBtn.addEventListener('click', ()=>{
-        stop=true
-    })   
-    startBtn.addEventListener('click', ()=>{
+    let interval 
+    function runGame(){
+        startBtn.removeEventListener('click', runGame)
+        grid.style.width= 'fit-content'
+        grid.style.height= 'fit-content'
         let str = boardInstance.render(board)
         grid.innerHTML= str
-        let interval = setInterval(() => {
+        interval = setInterval(() => {
             board= boardInstance.nextBoardState(board)
             str = boardInstance.render(board)
             grid.innerHTML= str
-            if(stop== true){
-                stop= false
-                clearInterval(interval)
-            }
+            // if(stop== true){
+            //     stop= false
+            //     clearInterval(interval)
+            // }
         }, 500);
-
+    }
+    stopBtn.addEventListener('click', ()=>{
+        stop=true
+        if(stop== true){
+            stop= false
+            clearInterval(interval)
+            startBtn.addEventListener('click', runGame)
+        }
+    })   
+    startBtn.addEventListener('click', runGame)
+    restartBtn.addEventListener('click', ()=>{
+        board= boardInstance.randomState(Math.floor(grid.clientWidth*0.08), Math.floor(grid.clientHeight*0.07))
+        stop= false
+        clearInterval(interval)
+        runGame()
     })
-
 })()
